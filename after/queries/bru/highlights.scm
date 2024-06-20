700f7141c03d7797e71ecea0702b9de19135e449
Pedro Barco <pedro.barco@tecnico.ulisboa.pt>
2024-06-20 10:38:58 +0100

    feat: add treesitter queries for highlights

---
+++ after/queries/bru/highlights.scm
@@ -0,0 +1,19 @@
+(keyword) @keyword
+
+[
+    "{"
+    "}"
+] @punctuation.bracket
+
+":" @punctuation.delimeter
+
+(key) @type
+
+[
+    (value)
+    (text)
+] @string
+
+(comment) @comment
+
+(ERROR) @error
